1d291d1b40ee610967bed61f4e48b7fe6365984e
4A-10\koyo <a>
2026-09-05 11:29:45 +0900

    SceneMenuのcppとhの追加

---
+++ SceneMenu.cpp
@@ -0,0 +1,19 @@
+#include "SceneMenu.h"
+
+#include "DxLib.h"
+
+
+void SceneMenu::Init()
+{
+	// ”wŒi‰æ‘œ‚Ì“Ç
+	this->bg0.Load_image("ed.png");
+}
+
+/// <summary>
+/// •`‰æˆ—
+/// </summary>
+void SceneMenu::Draw()
+{
+	// ”wŒi0‚ğ•`‰æ
+	this->bg0.Draw();
+}
